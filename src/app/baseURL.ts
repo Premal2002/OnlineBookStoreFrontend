b978e628a8860aa5643c052d76d0b92b681a1517
Premal2002 <114167095+Premal2002@users.noreply.github.com>
2025-02-06 13:29:18 +0530

    backend endpoint edit commit

--- src/app/baseURL.ts
+++ src/app/baseURL.ts
@@ -1,3 +1,4 @@
 //  export var baseurl = "http://booksheaven1.eu-north-1.elasticbeanstalk.com";
- export var baseurl = "http://localhost:5000";
+//  export var baseurl = "http://localhost:5000";
+export var baseurl = "https://onlinebookstorebackend-gik2.onrender.com";
 
